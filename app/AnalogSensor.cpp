27db713e0623f028dbf123396ead2c3823e579ba
PavanMantripragada <mppavan@umd.edu>
2022-10-12 22:55:19 +0000

    changes made

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -16,6 +16,11 @@ int AnalogSensor::Read()
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    // could have re-written the code
+    // without pointers but didn't
+    // want to change much of the code
+    delete readings;
+
     return result;
 }
 
